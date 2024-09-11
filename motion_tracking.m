1991f6f36ff51c08aa19883dc4d3b05739b155e7
jmdowrick <146781982+jmdowrick@users.noreply.github.com>
2024-09-12 10:12:31 +1200

    Split analysis/plotting + improve user inputs
    Folder and path handling at the start of the script are now more robust.

--- motion_tracking.m
+++ motion_tracking.m
@@ -3,38 +3,59 @@
 %% Set parameters
 framerate = 25; % frames per second
 dt = 1/framerate;
+search_rad = 10; % pixels (radius around glitter points to average)
 
 % Visualisation and saving toggle on/off
-playAnimation = true;
+playAnimation = false;
 saveAnimation = true;
 playAverageAnimation = false;
+playInterpAnimation = false;
 
 % Test features on / off
 interpolatedField = false; % doesn't work well without dense coverage of glitter
 
-% Define search information
-numGlitter_X = 5;
-numGlitter_Y = 3;
-grid_dim = [numGlitter_X,numGlitter_Y];
-search_rad = 10; % pixels (radius around glitter points to average)
+% Define paths for image files and for storing outputs
+folder_src = '/Users/jdow403/Desktop/AWB015_VID006';
+output_file_suffix = '_tracked';
 
-% Define paths for image files and saved outputs
-folder_main = '/Users/jdow403/Desktop/AWB015_VID006';
-folder_src = append(folder_main, '/images/');
-folder_outputs = append(folder_main, '/outputs');
-output_file_prefix = '/trackedcolon';
+if (~isfolder(folder_src) || numel(dir(fullfile(folder_src,'*.Bmp'))) == 0)
+    folder_src = uigetdir();
+end
+
+if numel(dir(fullfile(folder_src,'*.Bmp'))) == 0
+    error("Selected folder does not contain an image sequence. Please select a different folder.")
+end
+
+if exist(fullfile(folder_src,'outputs'), 'dir')
+    % check if processing has already been performed (does it contain .mat
+    % file?)
+    if numel(dir(fullfile(folder_src,'outputs','*.mat'))) > 0
+        % ask user if they want to repeat analysis
+        response = input("Motion tracking output detected, are you sure you want to proceed? (y/n) \n", "s");
+        if (lower(response) ~= "y" || isempty(response))
+            disp("Exiting motion tracking")
+            return
+        end
+    end
+else 
+    % If folder does not exist, create folder.
+    mkdir(fullfile(folder_src, 'outputs'))
+end
+
+folder_outputs = fullfile(folder_src, 'outputs');
 
 %% Load images and select region of interest
-dirList = dir(append(folder_src,'*.Bmp'));
+dirList = dir(fullfile(folder_src,'*.Bmp'));
 
 T = struct2table(dirList);
 dirList = natsortrows(T);
 dirList = table2struct(dirList);
 
-refImage = imread(append(dirList(1).folder, '/', dirList(1).name));
+refImage = imread(fullfile(dirList(1).folder, dirList(1).name));
 figure; imshow(refImage)
 h = size(refImage,1);
 w = size(refImage,2);
+title("Select region of interest and press Enter");
 
 % User selects region of interest (encapsulating markers)
 [~, roiBox] = imcrop(refImage);
@@ -43,14 +64,14 @@ close(gcf)
 % Display region of interest
 refImage_roi = insertShape(refImage, 'Rectangle', roiBox, 'Color', 'green');
 figure; imshow(refImage_roi);
-title("Selected Region of Interest");
+title("Selected region of interest");
 
 %% Feature tracking
 % Detect features to track
 points = detectMinEigenFeatures(im2gray(refImage), 'ROI', roiBox);
 figure; imshow(refImage);
 hold on; plot(points);
-title("Detected Features");
+title("Detected features");
 
 pts = round(points.Location);
 idx = sub2ind([h w], pts(:,2), pts(:,1));
@@ -73,7 +94,7 @@ videoPlayer = vision.VideoPlayer();
 % For loop through images in folder of interest
 for imNum = 1:length(dirList)
     % Load image
-    currentImage = imread(append(dirList(imNum).folder, '/', dirList(imNum).name));
+    currentImage = imread(fullfile(dirList(imNum).folder, dirList(imNum).name));
 
     [points, isFound, scores] = pointTracker(currentImage);
 
@@ -105,7 +126,7 @@ if playAnimation
     videoPlayer = vision.VideoPlayer();
 
     for imNum = 1:length(dirList)
-        currentImage = imread(append(dirList(imNum).folder, '/', dirList(imNum).name));
+        currentImage = imread(fullfile(dirList(imNum).folder, dirList(imNum).name));
         pts = [x(:,imNum), y(:,imNum)];
         currentImage = insertMarker(im2gray(currentImage), pts, '+', 'Color', 'green');
 
@@ -116,10 +137,10 @@ end
 
 %% Save animation
 if saveAnimation
-    video_object = VideoWriter(append(folder_outputs,output_file_prefix),'MPEG-4');
+    video_object = VideoWriter(fullfile(folder_outputs,output_file_suffix),'MPEG-4'); % add _tracked to end of folder name
     open(video_object);
     for imNum = 1:10:length(dirList)
-        currentImage = imread(append(dirList(imNum).folder, '/', dirList(imNum).name));
+        currentImage = imread(fullfile(dirList(imNum).folder, dirList(imNum).name));
         pts = [x(:,imNum), y(:,imNum)];
         currentImage = insertMarker(im2gray(currentImage), pts, '+', 'Color', 'green');
         writeVideo(video_object, currentImage);
@@ -147,8 +168,11 @@ close(fig);
 
 %% Group x and y displacement per point of glitter
 av_x = NaN(length(glitter_x), size(x,2));
-av_y = av_x;
-for i = 1:length(glitter_x)
+av_y = av_x; 
+
+% I don't think this works currently - needs to consider y aspect when
+% averaging per glitter point
+for i = 1:length(glitter_x) 
     temp_x = x(x(:,1)>=(glitter_x(i) - search_rad) & x(:,1) <= (glitter_x(i) + search_rad), :);
     av_x(i, :) = mean(temp_x);
 
@@ -163,7 +187,7 @@ if playAveragedAnimation
     videoPlayer = vision.VideoPlayer();
 
     for imNum = 1:length(dirList)
-        currentImage = imread(append(dirList(imNum).folder, '/', dirList(imNum).name));
+        currentImage = imread(fullfile(dirList(imNum).folder, dirList(imNum).name));
         pts = [av_x(:,imNum), av_y(:,imNum)];
         currentImage = insertMarker(im2gray(currentImage), pts, '+', 'Color', 'green');
         currentImage = insertShape(currentImage, 'circle', [av_x(:,imNum), av_y(:,imNum), repmat(search_rad,[size(av_x,1),1])], 'Color', 'green');
@@ -172,96 +196,20 @@ if playAveragedAnimation
     release(videoPlayer)
 end
 
-%% Calculate average y-displacement and x-displacement in the different axes
-
-av_x_sort = sortrows(av_x, 1);
-av_y_sort = sortrows(av_y, 1);
-
-for x_columns = 1:grid_dim(1)
-    x_disp(x_columns,:) = mean(av_x_sort((grid_dim(2)*(x_columns-1) + 1):(grid_dim(2)*(x_columns)),:),1);
-    x_disp_0(x_columns,:) = x_disp(x_columns,:) - x_disp(x_columns,1);
-end
-
-for y_columns = 1:grid_dim(2)
-    y_disp(y_columns,:) = mean(av_y_sort((grid_dim(1)*(y_columns-1) + 1):(grid_dim(1)*(y_columns)),:),1);
-    y_disp_0(y_columns,:) = y_disp(y_columns,:) - y_disp(y_columns,1);
-end
-
-y_disp_legend = y_disp(:,1)./ppmm;
-x_disp_legend = x_disp(:,1)./ppmm;
-
-%% Plot displacements - X axis
-colors = get(gca,'colororder');
-imNum = 1;
-currentImage = imread(append(dirList(imNum).folder, '/', dirList(imNum).name));
-pts = [av_x(:,imNum), av_y(:,imNum)];
-
-[av_x_sort, idx] = sortrows(av_x, 1);
-av_y_sort = av_y(idx,:);
-for i = 1:numGlitter_X
-    range = (numGlitter_Y*(i-1)+1):(numGlitter_Y*i);
-    currentImage = insertShape(currentImage, 'filled-circle', [av_x_sort(range,imNum), ...
-        av_y_sort(range,imNum), ...
-        repmat(search_rad,[numGlitter_Y,1])],'Opacity', 0.3, 'ShapeColor', colors(i,:), 'LineWidth', 2);
-end
-
-figure;
-subplot(1,2,1)
-hold on
-for i = 1:size(x_disp_0,1)
-    plot(time(1:end-50),x_disp_0(i,1:(end-50))./ppmm, 'LineWidth', 1.5)
-end
-xlabel('Time (s)')
-ylabel('x displacement (mm)')
-subplot(1,2,2)
-imshow(currentImage)
-fontsize(18,"points")
-
-%% Plot displacements - Y axis
-colors = get(gca,'colororder');
-imNum = 1;
-currentImage = imread(append(dirList(imNum).folder, '/', dirList(imNum).name));
-pts = [av_x(:,imNum), av_y(:,imNum)];
-
-[av_y_sort, idx] = sortrows(av_y, 1);
-av_x_sort = av_x(idx,:);
-
-for i = 1:numGlitter_Y
-    range = (numGlitter_X*(i-1)+1):(numGlitter_X*i);
-    currentImage = insertShape(currentImage, 'filled-circle', [av_x_sort(range,imNum), ...
-        av_y_sort(range,imNum), ...
-        repmat(search_rad,[numGlitter_X,1])],'Opacity', 0.3, 'ShapeColor', colors(i,:), 'LineWidth', 2);
-end
-
-figure;
-subplot(1,2,1)
-hold on
-for i = 1:size(y_disp_0,1)
-    plot(time(1:end-50),y_disp_0(i,1:end-50)./ppmm, 'LineWidth', 1.5)
-end
-xlabel('Time (s)')
-ylabel('y displacement (mm)')
-subplot(1,2,2)
-imshow(currentImage)
-fontsize(18,"points")
-
-%% Calculate x and y velocities
-[b,g] = sgolay(5,25);
-
-dx = zeros(size(x));
-dy = zeros(size(y));
-
-for i = 1:size(x,1)
-    dx(i,:) = conv(x(i,:), -1/dt * g(:,2), 'same');
-    dy(i,:) = conv(y(i,:), -1/dt * g(:,2), 'same');
-end
-
-%% Compare Savitzky-Golay derivative with naive differentiation
-dx_alt = diff(x,1,2)/dt;
-figure; hold on;  plot(dx_alt(5,20:(end-20))); plot(dx(5,20:(end-20)), 'LineWidth', 2);
-
 %% Interpolate velocities across rectangle
 if interpolatedField
+    % Calculate x and y velocities
+    [b,g] = sgolay(5,25);
+
+    dx = zeros(size(x));
+    dy = zeros(size(y));
+
+    for i = 1:size(x,1)
+        dx(i,:) = conv(x(i,:), -1/dt * g(:,2), 'same');
+        dy(i,:) = conv(y(i,:), -1/dt * g(:,2), 'same');
+    end
+    
+    % Interpolation (in development)
     border_size = 50;
     grid_size = 25;
 
@@ -283,17 +231,18 @@ if interpolatedField
 
     x_q = cumtrapz(dx_q, 2)*dt + repmat(Xq(:), [1, size(x,2)]);
     y_q = cumtrapz(dy_q, 2)*dt + repmat(Yq(:), [1, size(y,2)]);
+    
+    if playInterpAnimation
+        % Play animation of interpolated tracking
+        videoPlayer = vision.VideoPlayer();
 
-    % Play animation of interpolated tracking
-    videoPlayer = vision.VideoPlayer();
+        for imNum = 1:length(dirList)
+            currentImage = imread(fullfile(dirList(imNum).folder, dirList(imNum).name));
+            pts = [x_q(:,imNum), y_q(:,imNum)];
+            currentImage = insertMarker(im2gray(currentImage), pts, '+', 'Color', 'green');
 
-    for imNum = 1:length(dirList)
-        currentImage = imread(append(dirList(imNum).folder, '/', dirList(imNum).name));
-        pts = [x_q(:,imNum), y_q(:,imNum)];
-        currentImage = insertMarker(im2gray(currentImage), pts, '+', 'Color', 'green');
-
-        videoPlayer(currentImage)
+            videoPlayer(currentImage)
+        end
+        release(videoPlayer)
     end
-    release(videoPlayer)
-
 end
